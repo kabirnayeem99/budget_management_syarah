117b6567762eb61dbbbf38c18ecc69dfc62fe8c4
Naimul Kabir <kabirnayeem.99@gmail.com>
2023-05-26 18:50:06 +0600

    feat [transactions]: inject use cases into viewmodels

--- app/src/main/java/com/syarah/budgetmanagement/presentation/transactionDetails/TransactionDetailsViewModel.kt
+++ app/src/main/java/com/syarah/budgetmanagement/presentation/transactionDetails/TransactionDetailsViewModel.kt
@@ -1,8 +1,17 @@
 package com.syarah.budgetmanagement.presentation.transactionDetails
 
 import androidx.lifecycle.ViewModel
+import com.syarah.budgetmanagement.domain.usecase.transaction.DeleteTransactionUseCase
+import com.syarah.budgetmanagement.domain.usecase.transaction.GetTransactionsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class TransactionDetailsViewModel @Inject constructor(): ViewModel()
+class TransactionDetailsViewModel @Inject constructor(
+    private val getTransactionsUseCase: GetTransactionsUseCase,
+    private val deleteTransactionUseCase: DeleteTransactionUseCase,
+) : ViewModel() {
+
+
+
+}
--- app/src/main/java/com/syarah/budgetmanagement/presentation/transactionUpsert/TransactionUpsertViewModel.kt
+++ app/src/main/java/com/syarah/budgetmanagement/presentation/transactionUpsert/TransactionUpsertViewModel.kt
@@ -1,8 +1,15 @@
 package com.syarah.budgetmanagement.presentation.transactionUpsert
 
 import androidx.lifecycle.ViewModel
+import com.syarah.budgetmanagement.domain.usecase.transaction.GetTransactionDetailsUseCase
+import com.syarah.budgetmanagement.domain.usecase.transaction.UpdateTransactionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class TransactionUpsertViewModel @Inject constructor() : ViewModel()
+class TransactionUpsertViewModel @Inject constructor(
+    private val getTransactionDetailsUseCase: GetTransactionDetailsUseCase,
+    private val updateTransactionUseCase: UpdateTransactionUseCase,
+) : ViewModel() {
+
+}
