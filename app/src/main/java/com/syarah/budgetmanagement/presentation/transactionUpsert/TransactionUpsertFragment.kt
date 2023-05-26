ed9e870e30df68b348a6d8f70c61302b27ef7ec3
Naimul Kabir <kabirnayeem.99@gmail.com>
2023-05-26 21:06:39 +0600

    feat [transactions]: implement ui logic for transactions

--- app/src/main/java/com/syarah/budgetmanagement/presentation/transactionUpsert/TransactionUpsertFragment.kt
+++ app/src/main/java/com/syarah/budgetmanagement/presentation/transactionUpsert/TransactionUpsertFragment.kt
@@ -1,10 +1,21 @@
 package com.syarah.budgetmanagement.presentation.transactionUpsert
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.syarah.budgetmanagement.R
 import com.syarah.budgetmanagement.core.base.BaseFragment
 import com.syarah.budgetmanagement.databinding.FragmentTransactionUpsertBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TransactionUpsertFragment : BaseFragment<FragmentTransactionUpsertBinding>() {
@@ -12,4 +23,41 @@ class TransactionUpsertFragment : BaseFragment<FragmentTransactionUpsertBinding>
         get() = R.layout.fragment_transaction_upsert
 
     private val viewModel by activityViewModels<TransactionUpsertViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpViews()
+    }
+
+    private fun setUpViews() {
+        activity?.title = context?.getString(R.string.label_transaction_create_or_update)
+        setupMenu()
+        binding.apply {
+
+        }
+    }
+
+    private fun setupMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onPrepareMenu(menu: Menu) = Unit
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_transaction_upsert, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when (menuItem.itemId) {
+                    R.id.menu_save -> saveTransaction()
+                }
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun saveTransaction(){
+        lifecycleScope.launch {
+//            val transactionName = etN
+//            viewModel.createOrUpdateTransaction()
+        }
+    }
 }
